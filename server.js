30c1ecf09d83edd13ce754ae45eac98600e39253
Silvio Junior <silviommattos@gmail.com>
2016-11-12 19:02:06 -0200

    add telegram-api

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
-const express = require('express');
-var app = express();
+var TelegramBot = require('node-telegram-bot-api');
 
-app.listen(3000, function(){
-  console.log('listening in port 3000');
+var token = '266023933:AAFu-VG98d2ZneXhs0QF56c8R4xT7ee87B0';
+
+var bot = new TelegramBot(token, { polling: true });
+
+bot.on('message', function (msg) {
+  var chatId = msg.chat.id;
+  bot.sendMessage(chatId, "OK!");
 });
