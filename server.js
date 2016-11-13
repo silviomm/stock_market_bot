d5cf326673c4b823e63d51c3fd445d41569e331b
Silvio Junior <silviommattos@gmail.com>
2016-11-12 23:45:17 -0200

    test google

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 var TelegramBot = require('node-telegram-bot-api');
+var req = require('request');
 
 var token = '266023933:AAFu-VG98d2ZneXhs0QF56c8R4xT7ee87B0';
 
@@ -6,5 +7,10 @@ var bot = new TelegramBot(token, { polling: true });
 
 bot.on('message', function (msg) {
   var chatId = msg.chat.id;
+  req('http://google.com/search?q='+msg, function(error, resp, body){
+    if(!error && response.statusCode == 200){
+      bot.sendMessage(chatId, "200");
+    }
+  });
   bot.sendMessage(chatId, "OK!");
 });
