124b8646438ed3b2cddb732fb1cb9e40c78baabf
Silvio Junior <silviommattos@gmail.com>
2016-11-12 23:53:16 -0200

    test google

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var TelegramBot = require('node-telegram-bot-api');
 var req = require('request');
 
-var token = '266023933:AAFu-VG98d2ZneXhs0QF56c8R4xT7ee87B0';
+var token = '';
 
 var bot = new TelegramBot(token, { polling: true });
 
